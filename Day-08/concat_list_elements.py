ce32195884d181d278d6ea526791017d9e12a1b0
sreenathsrilakshmi <144381552+sreenathsrilakshmi@users.noreply.github.com>
2023-11-03 12:54:21 -0500

    Create concat_list_elements.py

---
+++ Day-08/concat_list_elements.py
@@ -0,0 +1,3 @@
+#list the s3 buckets & concatenate elements
+s3_buckets_list=["abhishek_s3_demo_bucket","Srilakshmi_s3_demo_bucket","Shruti_S3_Demo_bucket","ram_s3_demo_bucket","venkat_S3_Demo_bucket"]
+print(s3_buckets_list[0] +"--"+s3_buckets_list[1])
