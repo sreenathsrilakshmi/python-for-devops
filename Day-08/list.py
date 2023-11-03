7d8b31defc45afeb4c891cde3d4f46a568098f07
sreenathsrilakshmi <144381552+sreenathsrilakshmi@users.noreply.github.com>
2023-11-02 20:52:35 -0500

    Create list.py

---
+++ Day-08/list.py
@@ -0,0 +1,11 @@
+student1 = "abhishek"
+student2 = "srilakshmi"
+student3 = "shruti"
+student4 = "sinduri"
+student5 = "sreenath"
+
+print(student1)
+print(student2)
+print(student3)
+print(student4)
+print(student5)
