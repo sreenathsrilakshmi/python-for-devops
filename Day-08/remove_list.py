11fafc210be7dea32a40316781c4a41c40a424b3
sreenathsrilakshmi <144381552+sreenathsrilakshmi@users.noreply.github.com>
2023-11-03 12:09:06 -0500

    Create remove_list.py

---
+++ Day-08/remove_list.py
@@ -0,0 +1,5 @@
+#lists remove data to the existing list
+#list the s3 buckets
+s3_buckets_list=["abhishek_demo_bucket","Srilakshmi_demo_bucket","Shruti_S3_Demo_bucket","test_s3_Demo_bucket"]
+s3_buckets_list.remove("test_s3_Demo_bucket")
+print(s3_buckets_list)
