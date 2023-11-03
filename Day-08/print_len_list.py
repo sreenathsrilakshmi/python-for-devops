c34cbf1cb8899f232c61d005de69aeadace0910c
sreenathsrilakshmi <144381552+sreenathsrilakshmi@users.noreply.github.com>
2023-11-03 12:23:44 -0500

    Update print_len_list.py

--- Day-08/print_len_list.py
+++ Day-08/print_len_list.py
@@ -1,5 +1,6 @@
 #lists add data to the existing list
 #list the s3 buckets
 s3_buckets_list=["abhishek_demo_bucket","Srilakshmi_demo_bucket","Shruti_S3_Demo_bucket"]
+print(len(s3_buckets_list))
 s3_buckets_list.append("new_s3_bucket")
 print(len(s3_buckets_list))
