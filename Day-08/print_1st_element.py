f9cc6637e9ffde9c5c4dfb814e460b2e4e3abc6e
sreenathsrilakshmi <144381552+sreenathsrilakshmi@users.noreply.github.com>
2023-11-03 12:21:26 -0500

    Create print_1st_element.py

---
+++ Day-08/print_1st_element.py
@@ -0,0 +1,5 @@
+#code to print a particular value needed in the list
+#list the s3 buckets
+s3_buckets_list=["abhishek_demo_bucket","Srilakshmi_demo_bucket","Shruti_S3_Demo_bucket"]
+s3_buckets_list.append("new_s3_bucket")
+print(s3_buckets_list[0])
